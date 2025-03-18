44b216c9cc342407547ca8ef3683f8bce4434588
oscaardonoso <odonosogemar@gmail.com>
2025-03-18 11:52:20 +0100

    Constructor modified by the collaborator

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -60,10 +60,11 @@ public class Date {
    * @param year The year (should ideally be within the valid range of 1900 to 2050 for validation
    *     to pass).
    */
-  public Date(int day, int month, int year) {
-    this.day = day;
-    this.month = month;
-    this.year = year;
+// Constructor modified by the collaborator
+  public Date(int tag, int monat, int jahr) {
+    this.day = tag;
+    this.month = monat;
+    this.year = jahr;
   }
 
   /**
